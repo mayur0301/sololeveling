da5a869627197ebbcdf0cda3c61fde8b26d6beb6
Mayur <rajsalve2022@gmail.com>
2026-02-04 12:04:24 +0530

    Backend first commit of Solo Leveling

--- src/app.js
+++ src/app.js
@@ -15,11 +15,18 @@ const app = express()
 app.use(helmet())
 
 // CORS
+// app.use(cors({
+//   origin: process.env.CLIENT_URL,
+//   credentials: true
+// }))
 app.use(cors({
-  origin: process.env.CLIENT_URL,
-  credentials: true
-}))
+  origin: ["http://localhost:5173"],
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  exposedHeaders: ['Authorization']
 
+}))
 // Body parser
 app.use(express.json())
 
